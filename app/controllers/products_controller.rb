f59d175702435272975c0fdeae65dce6e708cb19
Mohamed Warsame <mwarsame9@gmail.com>
2017-07-14 11:25:32 -0700

    add show product path, AJAX not working yet

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,10 @@ class ProductsController < ApplicationController
     @order_item = current_order.order_items.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def new
     @product = Product.new
   end
