1386e21c75df7c7deb190fb4d71ab1f66f7c7750
Mohamed Warsame <mwarsame9@gmail.com>
2017-07-14 12:50:15 -0700

    Add product validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,20 @@
 class ProductsController < ApplicationController
-  before_filter :authorize, except: [:index, :show]
+  # before_filter :authorize, except: [:index, :show]
+
+  before_action :require_permission, only: :edit
+
+  before_action :only => [:edit] do
+    redirect_to product_path unless current_user && current_user.admin
+  end
+
+  def require_permission
+    if current_user != Product.find(params[:id]).user
+      redirect_to root_path
+    else
+      @product = Product.find(params[:id])
+      render :edit
+    end
+  end
 
   def index
     @products = Product.all
@@ -22,6 +37,28 @@ class ProductsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+def update
+  @product= Product.find(params[:id])
+  if @product.update(product_params)
+    flash[:notice] = "Product successfully updated!"
+    redirect_to products_path
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @product = Product.find(params[:id])
+  if @product.destroy
+    flash[:notice] = "Product successfully removed!"
+    redirect_to products_path
+  end
+end
 
   private
   def product_params
