f7a57a80bdf975290cc4ebb7ea5fc23ab2f07fd0
Mohamed Warsame <mwarsame9@gmail.com>
2017-07-21 16:32:16 -0700

    create a create js file in order_items so handle cart update AJAX

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,13 +3,16 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @item = @order.order_items.new(item_params)
-    @order.save
-      session[:order_id] = @order.id
-      flash[:notice] = "Product successfully added to cart!"
-        respond_to do |format|
-          format.html 
-          format.js { redirect_to products_path }
-        end
+    if @order.save
+      respond_to do |format|
+        session[:order_id] = @order.id
+        # flash[:notice] = "Product successfully added to cart!"
+        format.html
+        format.js
+      end
+    else
+     redirect_to products_path
+    end
   end
 
   def update
